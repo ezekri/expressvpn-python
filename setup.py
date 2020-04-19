4aaa6d9c495bc4b73193d237859b960a5b2bd0a7
ezekri <elyes.zekri@gmail.com>
2020-04-19 18:16:49 +0200

    Update setup.py
    Add package dependencie for web service

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='expressvpn-python',
     version='1.1',
     packages=['expressvpn'],
+    install_requires=['flask','flask_restful'],
     url='https://github.com/philipperemy/expressvpn-python',
     license='MIT',
     author='Philippe Remy',
